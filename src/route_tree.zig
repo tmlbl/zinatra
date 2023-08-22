d20422d3e4b068e11b7fbd701f9863dd6b384489
Tim Lebel <tim.lebel@gmail.com>
2023-08-21 23:22:40 -0400

    Broke Zig 0.10

--- src/route_tree.zig
+++ src/route_tree.zig
@@ -1,54 +1,70 @@
 const std = @import("std");
 
-pub fn RouteNode(comptime T: type) type {
+pub fn RouteTree(comptime T: type) type {
     return struct {
+        a: std.mem.Allocator,
         name: []const u8,
-        children: std.ArrayList(Self),
-        value: T,
+        children: std.ArrayList(*Self),
+        value: ?T,
 
         const Self = @This();
 
-        pub fn init(a: std.mem.Allocator, name: []const u8, value: T) !Self {
+        pub fn init(a: std.mem.Allocator, name: []const u8, value: ?T) Self {
             return Self{
+                .a = a,
                 .name = name,
-                .children = std.ArrayList(Self).init(a),
+                .children = std.ArrayList(*Self).init(a),
                 .value = value,
             };
         }
 
         pub fn deinit(self: *Self) void {
+            for (self.children.items) |child| {
+                child.deinit();
+                self.a.free(child);
+            }
             self.children.deinit();
         }
 
-        pub fn addChild(self: *Self, c: Self) !void {
-            try self.children.append(c);
-        }
+        pub fn add(self: *Self, path: []const u8, value: T) !void {
+            _ = value;
+            var it = std.mem.split(u8, path, "/");
+            var cur = self;
 
-        pub fn getChild(self: *Self, name: []const u8) ?Self {
-            for (self.children.items) |child| {
-                if (std.mem.eql(u8, child.name, name)) {
-                    return child;
+            while (it.next()) |part| {
+                if (part.len == 0) {
+                    continue;
+                }
+                if (!cur.hasChild(part)) {
+                    var new = Self.init(self.a, part, null);
+                    try cur.children.append(&new);
+                    cur = &new;
                 }
             }
-            return null;
+
+            std.debug.print("leaf node: {s}\n", .{cur.name});
+        }
+
+        fn hasChild(self: *Self, name: []const u8) bool {
+            for (self.children.items) |child| {
+                if (std.mem.eql(u8, child.name, name)) {
+                    return true;
+                }
+            }
+            return false;
         }
     };
 }
 
 const t = @import("testing");
 
-test "routenode" {
+test "RouteTree" {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer {
         _ = gpa.deinit();
     }
-    var r = try RouteNode(i32).init(gpa.allocator(), "foo", 12);
+    var r = RouteTree(i32).init(gpa.allocator(), "/", 12);
     defer r.deinit();
-    var child = try RouteNode(i32).init(gpa.allocator(), "bar", 13);
-    defer child.deinit();
 
-    std.debug.assert(r.getChild("bar") == null);
-
-    _ = try r.addChild(child);
-    std.debug.assert(std.mem.eql(u8, r.getChild("bar").?.name, child.name));
+    try r.add("/api/foo/bar", 37);
 }
