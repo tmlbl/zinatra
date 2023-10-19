3aabdcc1e1453f66e80d837f662b0a09133c3998
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-10-19 20:17:05 +0300

    create module

--- build.zig
+++ build.zig
@@ -15,6 +15,11 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    _ = b.addModule("zinatra", .{
+        .source_file = .{ .path = "src/App.zig" },
+        .dependencies = &[_]std.Build.ModuleDependency{},
+    });
+
     const lib = b.addStaticLibrary(.{
         .name = "zinatra",
         // In this case the main source file is merely a path, however, in more
